6b77dfc78b7aa41b14a0aaf74c142189fe81a304
陳鈞 <jim60105@gmail.com>
2024-10-12 08:03:18 +0800

    refactor: remove installGhCli function from bootstrap script
    - Remove `installGhCli` function and its call
    - Simplify `installCodeGPT` function call
    
    Signed-off-by: 陳鈞 <jim60105@gmail.com>

--- bootstrap.sh
+++ bootstrap.sh
@@ -4,11 +4,6 @@ cd "$(dirname "${BASH_SOURCE}")"
 
 git pull origin master
 
-function installGhCli() {
-	apk update
-	apk add -u -X \"https://dl-cdn.alpinelinux.org/alpine/edge/main\" -X \"https://dl-cdn.alpinelinux.org/alpine/edge/community\" github-cli
-}
-
 function installCodeGPT() {
 	gh run download --repo jim60105/CodeGPT -n amd64 -D /bin && sudo chmod 755 /bin/codegpt
 }
@@ -20,5 +15,5 @@ rsync --exclude ".git/" \
 	--exclude "README.md" \
 	--exclude "LICENSE" \
 	-avh --no-perms . ~
-installGhCli && installCodeGPT
+installCodeGPT
 source ~/.bash_profile
