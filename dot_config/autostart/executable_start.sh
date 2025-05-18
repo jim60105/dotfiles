bec7afbe31e0272c79936e960fa2122c7b622198
CHEN, CHUN <jim60105@gmail.com>
2025-05-19 02:04:59 +0800

    chore(adguard-cli): clear log files before starting adguard-cli
    - Before starting adguard-cli, the script now clears the contents of several log files to ensure fresh log files at each startup.
    
    Signed-off-by: CHEN, CHUN <jim60105@gmail.com>

--- dot_config/autostart/executable_start.sh
+++ dot_config/autostart/executable_start.sh
@@ -1,2 +1,8 @@
 #!/bin/bash
+# 在啟動 adguard-cli 之前清空日誌檔案
+truncate -s 0 /home/jim60105/.local/share/adguard-cli/access.log
+truncate -s 0 /home/jim60105/.local/share/adguard-cli/proxy.log
+truncate -s 0 /home/jim60105/.local/share/adguard-cli/proxy.log.*
+truncate -s 0 /home/jim60105/.local/share/adguard-cli/app.log
+
 adguard-cli start
