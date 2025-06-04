002eaa11ed0235cc160d291d234764e247fca64b
CHEN, CHUN <jim60105@gmail.com>
2025-06-04 23:45:35 +0800

    fix: fix for local devcontainer not working
    Signed-off-by: CHEN, CHUN <jim60105@gmail.com>

--- install.sh
+++ install.sh
@@ -21,10 +21,9 @@ installChezmoi() {
 		unset chezmoi_install_script bin_dir
 	fi
 
-	# POSIX way to get script's dir: https://stackoverflow.com/a/29834779/12156188
-	script_dir="$(cd -P -- "$(dirname -- "$(command -v -- "$0")")" && pwd -P)"
-
-	set -- init --apply --source="${script_dir}"
+	script="$(readlink --canonicalize-existing "$0")"
+	script_path="$(dirname "$script")"
+	set -- init --apply --source="${script_path}"
 
 	echo "Running 'chezmoi $*'" >&2
 	"$chezmoi" "$@"
