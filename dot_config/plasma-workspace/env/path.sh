78b8f7d38b28bf2e73a5ed2ca427bce24f5c7d67
Jim Chen <Jim@ChenJ.im>
2025-09-15 12:13:20 +0800

    feat: Add KITTY_DISABLE_WAYLAND environment variable
    Signed-off-by: Jim Chen <Jim@ChenJ.im>

--- dot_config/plasma-workspace/env/path.sh
+++ dot_config/plasma-workspace/env/path.sh
@@ -1,1 +1,2 @@
 export PATH=/home/jim60105/.local/share/zinit/polaris:/home/jim60105/.cargo/bin:/home/jim60105/.local/bin:/home/jim60105/bin:/usr/local/bin:/usr/bin:/bin:/usr/local/sbin:/usr/sbin:/sbin
+export KITTY_DISABLE_WAYLAND=1
